19a74f772c9230a8c710f9f8f471d0bb3f11e1ae
Elias Jr <eliasdecarvalhojunior@hotmail.com>
2020-10-04 11:18:24 -0300

    initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
             script: "./server.js",
             env: {
                 "PORT": 80,
-                "WATSON_URL": "https://api.us-south.assistant.watson.cloud.ibm.com/instances/9a4da6d7-793a-4b26-9032-20c3e92118df/v2/assistants/a3477a75-b6ad-4a8f-af5e-5f840ca6f482/sessions",
+                "WATSON_URL": "https://api.us-south.assistant.watson.cloud.ibm.com/instances/9a4da6d7-793a-4b26-9032-20c3e92118df",
                 "ASSISTANT_ID": "a3477a75-b6ad-4a8f-af5e-5f840ca6f482",
                 "WATSON_APIKEY": "sIA7V7rOVhEo1xv4oSWGNYChUT4QUrY79W02cCgYRwVr",
                 "WATSON_VERSION": "2020-04-01",
